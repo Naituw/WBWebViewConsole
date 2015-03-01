80f59dbe68ae638141d9e9dd879217280e5b4f04
Wutian <naituw@gmail.com>
2015-03-02 01:18:15 +0800

    Update License Headers for Unit Test sources

--- WBWebViewConsoleTests/Base/WBTestsUIWebView.m
+++ WBWebViewConsoleTests/Base/WBTestsUIWebView.m
@@ -3,7 +3,12 @@
 //  WBWebViewConsole
 //
 //  Created by 吴天 on 15/3/1.
-//  Copyright (c) 2015年 Sina. All rights reserved.
+//
+//  Copyright (c) 2014-present, Weibo, Corp.
+//  All rights reserved.
+//
+//  This source code is licensed under the BSD-style license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 #import "WBTestsUIWebView.h"
